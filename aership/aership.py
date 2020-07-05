1f2b2fd649390cebf6d6bc4713379b381b0e6a05
C. R <bigfoot.c@hotmail.de>
2020-07-05 23:53:59 +0200

    Added function to computer auxilliary weights (fuel system, power plant, ballast, crew, etc) based on computation of total air weight.

--- aership/aership.py
+++ aership/aership.py
@@ -69,9 +69,10 @@ def calculate_horsepower_using_standard_displacement(displacement, density_of_ai
     horsepower =  (( displacement ** 2/3 ) * density_of_air * ( speed ** 3 ) ) / ( 99 * design_coefficient )
     return horsepower
 
-#Trying to calculate a problem set from the book
-
-"""
+def calculate_total_weight_of_air_and_gas( 
+    percentage_filled_total_volume, 
+    lift_per_unit_volume, standard_air_weight):
+    """
    Problem 1
    Find the volume and horsepower of a rigid airship to carry a military load of 15,000 lbs.
    at 60 knots (101.3 ft. / sec.). for 60 hours, 85% of the total volume being filled with helium
@@ -82,9 +83,6 @@ def calculate_horsepower_using_standard_displacement(displacement, density_of_ai
    the lift of gas per unit volume, and divided by the weight of air per unit volume. The total
    weight of air and gas is therefore ...
 """
-def calculate_total_weight_of_air_and_gas( 
-    percentage_filled_total_volume, 
-    lift_per_unit_volume, standard_air_weight):
 
     # Entire equation
     #total_air_weight =  ( (100 - percentage_filled_total_volume) / 100.0 ) + \
@@ -101,4 +99,18 @@ def calculate_total_weight_of_air_and_gas(
     return round(total_air_weight, 5)
 
 my_ttaw = calculate_total_weight_of_air_and_gas( 85.0, 0.064, standard_air_weight )
-print( my_ttaw )
+print("Total weight of air and gas is: ", my_ttaw )
+
+def compute_auxilliary_weight(total_air_weight, 
+                              fixed_weights_power = 0.30, 
+                              fixed_weights_ballast_and_crew = 0.055):
+    """
+    From existing data, the fixed weights exclusive of power plant, 
+    power cars, and fuel system equal -.3D, and the crew, 
+    stores and ballast amount to .055 D. 
+    There remains for power plant, fuel, and military load
+    """
+    return round(1 - total_air_weight - fixed_weights_power - fixed_weights_ballast_and_crew, 5)
+
+my_auxilliary_weight = compute_auxilliary_weight(my_ttaw)
+print( "Weight for power plant, fuel and military load is: ", my_auxilliary_weight )
