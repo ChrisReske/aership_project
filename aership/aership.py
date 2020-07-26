4c113c89ef0ace54c5f6ea5124cda2b29ff2d177
C. R <bigfoot.c@hotmail.de>
2020-07-26 12:42:56 +0200

    Moved function for calculating total weight of air and gas (based on problem set) into a separate file  to be imported into main application.

--- aership/aership.py
+++ aership/aership.py
@@ -1,5 +1,6 @@
 import standard_cubic_weight
 import horsepower
+import total_weight
 
 """
    Main()
@@ -8,37 +9,8 @@ import horsepower
 standard_air_weight = standard_cubic_weight.get_standard_weight()
 aership_horsepower = horsepower.calculate_horsepower(200, 2, 5, 50)
 print( "aership_hp: " + str( aership_horsepower) )
-
-def calculate_total_weight_of_air_and_gas( 
-    percentage_filled_total_volume, 
-    lift_per_unit_volume, standard_air_weight):
-    """
-   Problem 1
-   Find the volume and horsepower of a rigid airship to carry a military load of 15,000 lbs.
-   at 60 knots (101.3 ft. / sec.). for 60 hours, 85% of the total volume being filled with helium
-   lifting .064 lb./ft.**3 (94 % pure) in the standard atmosphere.
-
-   Since the hull is specified in the condition of the problem to be 85% full of gas, 
-   the weight of the gas is 85% D multiplied by the difference between the weight of air and
-   the lift of gas per unit volume, and divided by the weight of air per unit volume. The total
-   weight of air and gas is therefore ...
-"""
-
-    # Entire equation
-    #total_air_weight =  ( (100 - percentage_filled_total_volume) / 100.0 ) + \
-    #                    ( ( percentage_filled_total_volume * ( standard_air_weight - lift_per_unit_volume ) )\
-    #                   / standard_air_weight )
-
-    ttaw_equation_01 = ( (100 - percentage_filled_total_volume) / 100.0 ) 
-    ttaw_tmp_result_02 =( ( percentage_filled_total_volume / 100.0 ) * standard_air_weight )
-    ttaw_tmp_result_03 = ( ( percentage_filled_total_volume / 100.0 ) * ( lift_per_unit_volume ) * ( -1.0 ) )
-    # Using results from first binomial block for second block (division)
-    ttaw_tmp_result_04 = ttaw_tmp_result_02 / standard_air_weight
-    ttaw_tmp_result_05 = ttaw_tmp_result_03 / standard_air_weight
-    total_air_weight = ttaw_equation_01 + ttaw_tmp_result_04 + ttaw_tmp_result_05
-    return round(total_air_weight, 5)
-
-my_ttaw = calculate_total_weight_of_air_and_gas( 85.0, 0.064, standard_air_weight )
+# Calculate total weight of air and gas
+my_ttaw = total_weight.calculate_total_weight_of_air_and_gas( 85.0, 0.064, standard_air_weight )
 print("Total weight of air and gas is: ", my_ttaw )
 
 def compute_auxilliary_weight(total_air_weight, 
