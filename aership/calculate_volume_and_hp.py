d998a2c87817a1a31d35f85b5f99e9c30567d44c
C. R <bigfoot.c@hotmail.de>
2020-08-16 14:48:11 +0200

    Started function to calculate problem set 1 from b ook, setting values from book as default parameters for function.

---
+++ aership/calculate_volume_and_hp.py
@@ -0,0 +1,52 @@
+import total_weight
+
+def calculate_vol_and_hp( load, speed=50.0, operating_hours=60.0, fill_volume=100, lift_capacity=0.064 ):
+    """
+    Calculates volume and horsepower of a rigid airship taking into account a user-defined
+    military or commercial load, a user-defined speed in knots, user-defined hours for operating
+    on the speed defined in this function as well as the total volume the airship is supposed to be
+    filled with.
+    Returns volume in ft. cubed
+
+    Parameters
+    --------------
+    load:               decimal
+                        User-defined military or commercial load in lbs, the airship is expected to carry.
+
+    speed:              decimal
+                        User-defined speed in knots the airships is suppose to operate.
+                        If not indicated otherwise, the default speed is 50 knots.
+
+    operating_hours:    decimal
+                        User-defined hours for which the speed defined in parameter 'speed' should be maintained.
+                        If not specificed, a default of 60 hourse will be used
+    
+    fill_volume:        integer
+                        The total volume being fille for the calculation.
+                        Use integers to denote the percentage, e.g. 85 for 85% of the airship volume being filled.
+                        If not indicated, the default of 100 = 100% will be used
+
+    lift_capacity:      decimal
+                        Lift of lb per cubed feet depending on gas used and its purity in the standard atmosphere.
+                        If not indicated otherwise, a default of 0.064 lb/ft**3 using 94 % pure helium is used.
+
+    The book author just eliminated Hp from 1 by the formula 2.
+    15000+44(.39D2/3)=.357D
+
+    Do arthitmatic and rearrange:
+
+    .357D−17.16D2/3=42000
+
+    Divide by the .357 to get
+
+    D−48.067 ** D2/3 =42016.8
+
+    Then round off because he used a slide rule not a calculator.
+
+
+    """
+
+    # First compute ttotla weight of air and gas
+    tw_air_and_gas = total_weight.calculate_total_weight_of_air_and_gas(fill_volume, )
+
+
